33a6cbffee0ed1b229ab1daf6e1f6a248c5e8602
Austn Hughes <austen.hughes.91@gmail.com>
2021-04-20 22:36:28 -0600

    file set up

---
+++ models/index.js
@@ -0,0 +1,24 @@
+const Post = require('./post');
+const User = require('./user');
+const Comments = require('./comments')
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+Comments.belongsTo(Post, {
+    foreignKey: 'post_id',
+});
+
+Post.hasMany(Comments,{
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
+});
+  
+
+module.exports = { User, Post, Comments };
